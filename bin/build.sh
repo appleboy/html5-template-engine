25f2992cea3b29a815e434d67f089c30d95a5d94
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-01-14 10:46:22 +0800

    add handlebars and requirejs
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -8,8 +8,8 @@ echo '=> install rubygems tool'
 aptitude -y install ruby rake rubygems
 echo '=> update rubygems tool'
 gem update --system
-echo '=> install compass tool'
-gem install compass
+echo '=> install compass and sass tool'
+gem install sass compass
 echo '=> install livereload tool'
 gem install guard-livereload
 
@@ -28,3 +28,7 @@ echo '=> install coffee-script tool'
 npm install -g coffee-script
 echo '=> install transfer javascript to coffee script tool'
 npm install -g js2coffee
+echo '=> install template engine'
+npm install -g handlebars
+echo '=> install RequireJS in Node'
+npm install -g requirejs
