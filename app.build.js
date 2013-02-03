f23d19531b7911c88818d838bbba8b6e0f44ab2c
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-02-03 19:59:11 +0800

    add build script
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ app.build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: "assets/js/",
+    name: "main",
+    out: "assets/js/main-built.js",
+    mainConfigFile: "assets/js/main.js",
+    preserveLicenseComments: false,
+    paths: {
+        jquery: "vendor/jquery/jquery"
+    }
+})
