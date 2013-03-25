811ef5f77f5d3f636ebfdf8e04bc92337a4bda96
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-03-25 22:50:36 +0800

    fixed bower command error and add command color.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -3,22 +3,27 @@
 PATH=$PATH:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
 export PATH
 
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[32m"
+RESET="\033[0m"
+
 ##
 # install gem develop tool
 ##
 
 which gem 1> /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo '=> Please install gem tool via aptitude command.'
-    echo '=> $ sudo aptitude -y install ruby rake rubygems'
+    echo -e "${YELLOW}=> Please install gem tool via aptitude command.${RESET}"
+    echo -e "${YELLOW}=> $ sudo aptitude -y install ruby rake rubygems${RESET}"
     exit
 fi
 
 which compass 1> /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo '=> update rubygems tool'
+    echo -e "${YELLOW}=> update rubygems tool.${RESET}"
     sudo gem update --system
-    echo '=> install compass and sass tool'
+    echo -e "${YELLOW}=> install compass and sass tool.${RESET}"
     sudo gem install sass compass
 fi
 
@@ -28,31 +33,32 @@ fi
 
 which npm 1> /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo '=> install nvm tool'
+    echo -e "${YELLOW}=> install nvm tool.${RESET}"
     curl https://raw.github.com/appleboy/nvm/develop/install.sh | sh
     . ~/.nvm/nvm.sh # This loads NVM
-    echo '=> install node.js stable version'
+    echo -e "${YELLOW}=> install node.js stable version.${RESET}"
     nvm install stable
-    echo '=> use node.js stable version'
+    echo -e "${YELLOW}=> use node.js stable version.${RESET}"
     nvm use stable
 fi
 
-echo '=> install coffee-script tool'
+echo -e "${YELLOW}=> install coffee-script tool${RESET}"
 which coffee 1> /dev/null 2>&1 || npm install -g coffee-script
-echo '=> install RequireJS in Node'
+echo -e "${YELLOW}=> install RequireJS in Node${RESET}"
 which r.js 1> /dev/null 2>&1 || npm install -g requirejs
-echo '=> install bower tool ref: http://twitter.github.com/bower/'
+echo -e "${YELLOW}=> install bower tool ref: http://twitter.github.com/bower/${RESET}"
 which bower 1> /dev/null 2>&1 || npm install -g bower
-echo '=> install express server ref: http://expressjs.com'
+echo -e "${YELLOW}=> install express server ref: http://expressjs.com${RESET}"
 which express 1> /dev/null 2>&1 || npm install -g express
-echo '=> install UglifyJS 2 node-based js compressor.'
+echo -e "${YELLOW}=> install UglifyJS 2 node-based js compressor.${RESET}"
 which uglifyjs 1> /dev/null 2>&1 || npm install -g uglify-js
-echo '=> install sqwish node-based CSS compressor.'
+echo -e "${YELLOW}=> install sqwish node-based CSS compressor.${RESET}"
 which sqwish 1> /dev/null 2>&1 || npm install -g sqwish
-echo '=> install django-html in an HTML minifier'
-which pyminify 1> /dev/null 2>&1 || pip install django-htmlmin
+echo -e "${YELLOW}=> install django-html in an HTML minifier.${RESET}"
+which pyminify 1> /dev/null 2>&1 || sudo pip install django-htmlmin
 
 # install npm dependency packages for grunt.js.
 npm install
+
 # install require bower dependency package.
-bower installl
+bower install
