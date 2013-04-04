f93beda61fb5fc4ce530c30bd2abc4358910234b
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-04-04 19:47:05 +0800

    move compass config file to top folder.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+http_path = "/app/"
+css_dir = "/app/assets/css"
+sass_dir = "/app/assets/sass"
+images_dir = "/app/assets/images"
+javascripts_dir = "/app/assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
