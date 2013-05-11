1237c138b91edeb1f9965268ab567989a8c1cfa1
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-05-11 19:20:38 +0800

    install mocha test tool.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -70,6 +70,8 @@ display "install django-html in an HTML minifier."
 which pyminify 1> /dev/null 2>&1 || sudo pip install django-htmlmin
 display 'install grunt tool.'
 which grunt 1> /dev/null 2>&1 || npm install -g grunt-cli
+display 'install mocha tool.'
+which mocha 1> /dev/null 2>&1 || npm install -g mocha
 
 # install npm dependency packages for grunt.js.
 npm install
