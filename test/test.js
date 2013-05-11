ba0239798000bce55c9eed8e4e1d3074f63cd69d
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-05-11 21:46:53 +0800

    update test script.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
-var assert = require("assert")
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
+var assert = require("assert");
+var fs = require('fs');
+
+describe('html5 template generator test', function() {
+    it('creates expected files in AMD mode', function() {
+        var expected = [
+            'output/favicon.ico',
+            'output/.htaccess',
+            'output/index.html',
+            'output/404.html',
+            'output/robots.txt',
+            'output/assets/js/require.js',
+            'output/assets/css/print.css',
+            'output/assets/css/screen.css',
+            'output/assets/css/ie.css'
+        ];
+        for(var key in expected) {
+            assert.equal(true, fs.existsSync(expected[key]));
+        }
     })
 })
