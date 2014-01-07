25069b7e910be7f0f2178525a4702f8fbad98f69
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-01-07 10:44:04 +0800

    support with gulp jshint plugin.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
     lr = require('tiny-lr'),
     refresh = require('gulp-livereload'),
     server = lr(),
-    w3cjs = require('gulp-w3cjs');
+    w3cjs = require('gulp-w3cjs'),
+    jshint = require('gulp-jshint');
 
 gulp.task('coffee', function() {
     gulp.src('app/assets/coffeescript/**/*.coffee')
@@ -38,13 +39,53 @@ gulp.task('compass', function() {
         .pipe(refresh(server));
 });
 
+gulp.task('lint', function() {
+    gulp.src('gulpfile.js')
+        .pipe(jshint({
+            'node': true,
+            'esnext': true,
+            'bitwise': true,
+            'camelcase': false,
+            'curly': true,
+            'eqeqeq': true,
+            'immed': true,
+            'indent': 4,
+            'latedef': true,
+            'newcap': true,
+            'noarg': true,
+            'quotmark': 'single',
+            'regexp': true,
+            'undef': true,
+            'unused': true,
+            'strict': true,
+            'trailing': true,
+            'smarttabs': true,
+            'globals': {
+                /* MOCHA */
+                'describe'   : false,
+                'it'         : false,
+                'before'     : false,
+                'beforeEach' : false,
+                'after'      : false,
+                'afterEach'  : false,
+                'require'    : false
+            }
+        }))
+        .pipe(jshint.reporter('default'));
+});
+
 // The default task (called when you run `gulp`)
 gulp.task('default', function() {
+    gulp.run('lint');
+
     // Watch files and run tasks if they change
     server.listen(35729, function (err) {
         if (err) {
             return console.log(err);
         }
+        gulp.watch('gulpfile.js', function() {
+            gulp.run('lint');
+        });
         gulp.watch('app/assets/coffeescript/**/*.coffee', function() {
             gulp.run('coffee');
         });
