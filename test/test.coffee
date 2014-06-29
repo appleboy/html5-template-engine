f0293205f8753c46f2d69e20970ebd3311edd9f7
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-06-29 22:32:27 +0800

    update test script.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/test.coffee
+++ test/test.coffee
@@ -4,15 +4,13 @@ fs = require('fs')
 describe 'html5 template generator test', ->
   it 'creates expected files', ->
     expected = [
-      'dist/favicon.ico',
-      'dist/.htaccess',
-      'dist/index.html',
-      'dist/404.html',
-      'dist/robots.txt',
-      'dist/assets/js/require.js',
-      'dist/assets/css/print.css',
-      'dist/assets/css/screen.css',
-      'dist/assets/css/ie.css',
+      'dist/favicon.ico'
+      'dist/.htaccess'
+      'dist/index.html'
+      'dist/404.html'
+      'dist/robots.txt'
+      'dist/assets/js/require.js'
+      'dist/assets/css/main.css'
       'dist/assets/images/chrome.png'
     ]
     for item in expected
@@ -20,9 +18,11 @@ describe 'html5 template generator test', ->
 
   it 'delete expected files', ->
     not_expected = [
-      'dist/assets/vendor/',
-      'dist/assets/coffee/',
+      'dist/assets/vendor/'
+      'dist/assets/coffee/'
       'dist/assets/sass/'
+      'dist/assets/css/ie.css'
+      'dist/assets/css/print.css'
     ]
     for item in not_expected
       assert.equal(false, fs.existsSync(item))
--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ fs = require('fs');
 describe('html5 template generator test', function() {
   it('creates expected files', function() {
     var expected, item, _i, _len, _results;
-    expected = ['dist/favicon.ico', 'dist/.htaccess', 'dist/index.html', 'dist/404.html', 'dist/robots.txt', 'dist/assets/js/require.js', 'dist/assets/css/print.css', 'dist/assets/css/screen.css', 'dist/assets/css/ie.css', 'dist/assets/images/chrome.png'];
+    expected = ['dist/favicon.ico', 'dist/.htaccess', 'dist/index.html', 'dist/404.html', 'dist/robots.txt', 'dist/assets/js/require.js', 'dist/assets/css/main.css', 'dist/assets/images/chrome.png'];
     _results = [];
     for (_i = 0, _len = expected.length; _i < _len; _i++) {
       item = expected[_i];
@@ -17,7 +17,7 @@ describe('html5 template generator test', function() {
   });
   it('delete expected files', function() {
     var item, not_expected, _i, _len, _results;
-    not_expected = ['dist/assets/vendor/', 'dist/assets/coffee/', 'dist/assets/sass/'];
+    not_expected = ['dist/assets/vendor/', 'dist/assets/coffee/', 'dist/assets/sass/', 'dist/assets/css/ie.css', 'dist/assets/css/print.css'];
     _results = [];
     for (_i = 0, _len = not_expected.length; _i < _len; _i++) {
       item = not_expected[_i];
